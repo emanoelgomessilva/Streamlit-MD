89a8ad2240acd650b581f23a58a6e0d8a2b05f5e
Pablo Renato <56098864+pablorenato1@users.noreply.github.com>
2023-08-21 21:51:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import streamlit as st
 import plotly_express as px
 import plotly.graph_objects as go
 
-conn = st.experimental_connection('mysql', type='sql')
-#Carregar tabelas
-avaliacoes = conn.query('SELECT * from dimavaliacoes;', ttl=600)
-usuarios = conn.query('SELECT * from dimusuario;', ttl=600)
-datas = conn.query('SELECT * from dimdatas;', ttl=600)
-locais = conn.query('SELECT * from dimlocais;', ttl=600)
-fatos = conn.query('SELECT * from fatonota;', ttl=600)
-#Transformar em dataframes e excluir colunas
-df_avaliacoes = pd.DataFrame(avaliacoes).drop(['version','date_to','date_from'], axis=1)
-df_usuarios = pd.DataFrame(usuarios).drop(['version','date_to','date_from'],axis=1)
-df_datas = pd.DataFrame(datas).drop(['version','date_to','date_from'],axis=1)
-df_locais = pd.DataFrame(locais).drop(['version','date_to','date_from'],axis=1)
-df_fatos = pd.DataFrame(fatos)
-#Merge
-df_fatos = pd.merge(df_avaliacoes,df_fatos, left_on='keyAvaliacao', right_on='key_avaliacao')
-df_fatos = pd.merge(df_usuarios,df_fatos, left_on='keyUsuario', right_on='key_usuario')
-df_fatos = pd.merge(df_datas, df_fatos, left_on='keyData', right_on='key_data')
-df_fatos = pd.merge(df_locais,df_fatos, left_on='keyLocal', right_on='key_local',how='left').dropna()
-df_fatos.rename(columns={'Q1':"Segurança", 'Q2':"Infraestrutura", 'Q3':"Limpeza", 'Q4':"Funcionarios", 'Q5':"Localizacao", 'Q6':"Recursos",'Q7':"Atendimento", 'Q8':"Serviços", 'Q9':"Interatividade", 'Q10':"Preços"}, inplace=True)
-df_fatos['ano_id'] = df_fatos['ano_id'].astype(int)
+# conn = st.experimental_connection('mysql', type='sql')
+# #Carregar tabelas
+# avaliacoes = conn.query('SELECT * from dimavaliacoes;', ttl=600)
+# usuarios = conn.query('SELECT * from dimusuario;', ttl=600)
+# datas = conn.query('SELECT * from dimdatas;', ttl=600)
+# locais = conn.query('SELECT * from dimlocais;', ttl=600)
+# fatos = conn.query('SELECT * from fatonota;', ttl=600)
+# #Transformar em dataframes e excluir colunas
+# df_avaliacoes = pd.DataFrame(avaliacoes).drop(['version','date_to','date_from'], axis=1)
+# df_usuarios = pd.DataFrame(usuarios).drop(['version','date_to','date_from'],axis=1)
+# df_datas = pd.DataFrame(datas).drop(['version','date_to','date_from'],axis=1)
+# df_locais = pd.DataFrame(locais).drop(['version','date_to','date_from'],axis=1)
+# df_fatos = pd.DataFrame(fatos)
+# #Merge
+# df_fatos = pd.merge(df_avaliacoes,df_fatos, left_on='keyAvaliacao', right_on='key_avaliacao')
+# df_fatos = pd.merge(df_usuarios,df_fatos, left_on='keyUsuario', right_on='key_usuario')
+# df_fatos = pd.merge(df_datas, df_fatos, left_on='keyData', right_on='key_data')
+# df_fatos = pd.merge(df_locais,df_fatos, left_on='keyLocal', right_on='key_local',how='left').dropna()
+# df_fatos.rename(columns={'Q1':"Segurança", 'Q2':"Infraestrutura", 'Q3':"Limpeza", 'Q4':"Funcionarios", 'Q5':"Localizacao", 'Q6':"Recursos",'Q7':"Atendimento", 'Q8':"Serviços", 'Q9':"Interatividade", 'Q10':"Preços"}, inplace=True)
+# df_fatos['ano_id'] = df_fatos['ano_id'].astype(int)
 
 #Barra lateral
+df_fatos = pd.read_csv('basefull.csv')
 #Tipo de local
 st.sidebar.header('Filtre seus resultados')
 local_types = df_fatos['tipo'].unique().tolist()
